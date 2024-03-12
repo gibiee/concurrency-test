b7020bfbd49a7c2ce97f2fa1479befb08c0b989a
gibiee <gibiee@naver.com>
2024-03-12 19:42:18 +0900

    add gpu demo

--- README.md
+++ README.md
@@ -33,11 +33,11 @@ pip install gradio uvicorn
 - 데모의 `concurrency_limit` 속성은 데모 페이지에서 버튼을 누를 때만 적용되고 API 호출과는 무관하다.
   - 쓰레드 기반으로 동작함. 따라서, **button2(연산)** 기능을 동시에 사용하면 모든 요청의 속도가 느려짐.
 - 데모 및 API 함수 모두에 `async def`를 적용해야, 데모 페이지에서 기능 실행 중에 API 호출이 동시 실행되는 현상을 방지할 수 있음.
-- 따라서, 동기적 처리를 원한다면 데모 및 API 모든 함수에서 `async def`를 사용해야함!
+- ~~따라서, 동기적 처리를 원한다면 데모 및 API 모든 함수에서 `async def`를 사용해야함!~~
   - 대신, 해당 함수를 호출 시 `await`를 사용해야함. 그렇지 않으면, 데모와 API에서 동시 호출 시 `TypeError("'coroutine' object is not iterable")`에러가 발생함.
   - 대신, gradio 데모 페이지에서 queue 대기열이 얼마나 되는지 미리 확인할 수 없음.
   - 데모와 API 양쪽에서 동시 호출했을 때 API 호출이 더 늦었음에도 불구하고 데모보다 우선적으로 처리됨.
-  - **하지만 `async def`를 사용하는 경우, request 자체가 대기하므로 timeout 에러가 발생할 수 있음.**
+  - **하지만 `async def`를 사용하는 경우, request 자체가 대기하므로 요청 보내는 쪽에서 timeout 에러가 발생할 수 있음.**
 - `uvicorn`과 `gunicorn` 모두 프로세스 기반으로 동작한다.
   - `uvicorn test:app --workers 3`으로 **button2(연산)** 을 수행한 결과, 모든 요청이 약 8초 소요되었음.
   - `gunicorn test:app --worker-class uvicorn.workers.UvicornWorker --workers 3`으로 **button2(연산)** 을 수행한 결과, 모든 요청이 약 8초 소요되었음.
--- test_gpu.py
+++ test_gpu.py
@@ -1,11 +1,42 @@
+import os
+os.environ['CUDA_VISIBLE_DEVICES']='1'
+
 from diffusers import DiffusionPipeline
 import torch
+import gradio as gr
+from fastapi import FastAPI
+import time
+
+app = FastAPI()
+concurrency_limit = 1
 
 pipe = DiffusionPipeline.from_pretrained("stabilityai/stable-diffusion-xl-base-1.0", torch_dtype=torch.float16, use_safetensors=True, variant="fp16")
 pipe.to("cuda")
 
-prompt = "An astronaut riding a green horse"
-images = pipe(prompt=prompt, num_inference_steps=20).images
-print(images)
-image = images[0]
-print(image)
+@app.get("/generate")
+def api_generate() :
+    result = generate_image()
+    return {"result": "DONE"}
+
+def generate_image() :
+    print('Start generating !')
+
+    images = pipe(prompt="An astronaut riding a green horse", num_inference_steps=20).images
+    image = images[0]
+    print(image)
+
+    print('Finish generating !')
+    return image
+
+with gr.Blocks() as demo :
+    with gr.Row() :
+        with gr.Column() :
+            input_text = gr.Textbox(label='input', value="An astronaut riding a green horse")
+            button = gr.Button(value='button', variant="primary")
+
+        output_img = gr.Image(label='output')
+    
+    button.click(fn=generate_image, outputs=output_img, concurrency_limit=concurrency_limit)
+
+demo.launch()
+# app = gr.mount_gradio_app(app, demo, path="/")
